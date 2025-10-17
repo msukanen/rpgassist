20b2013dbdaa10100d084f9486915828386ec1e7
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-17 15:25:00 +0300

    Bilateral randomness.

--- src/direction/bilateral.rs
+++ src/direction/bilateral.rs
@@ -1,6 +1,17 @@
+use dicebag::DiceExt;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, Hash)]
 pub enum Bilateral {
     Left, Right,
+}
+
+impl Bilateral {
+    /// Generate left/right randomly.
+    pub fn left_or_right() -> Self {
+        match 1.d2() {
+            1 => Self::Left,
+            _ => Self::Right
+        }
+    }
 }
