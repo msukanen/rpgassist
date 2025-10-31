aa32da3d3b86ea21616afc1d70f1a08f0d03c89e
Markku Sukanen <markku.sukanen@gmail.com>
2025-10-31 21:18:13 +0200

    New bilaterals. Refactor left_or_right -> random_lr. Added random_fb and impl Display.

--- src/direction/bilateral.rs
+++ src/direction/bilateral.rs
@@ -1,17 +1,40 @@
+use std::fmt::Display;
+
 use dicebag::DiceExt;
 use serde::{Deserialize, Serialize};
 
+/// Some bilateralisms…
 #[derive(Debug, Deserialize, Serialize, Hash, Clone)]
 pub enum Bilateral {
     Left, Right,
+    Front, Back,
 }
 
 impl Bilateral {
     /// Generate left/right randomly.
-    pub fn left_or_right() -> Self {
+    pub fn random_lr() -> Self {
         match 1.d2() {
             1 => Self::Left,
             _ => Self::Right
         }
+    }
+
+    /// Generate front/back randomly.
+    pub fn random_fb() -> Self {
+        match 1.d2() {
+            1 => Self::Front,
+            _ => Self::Back
+        }
+    }
+}
+
+impl Display for Bilateral {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::Back => "back",
+            Self::Front => "front",
+            Self::Left => "left",
+            Self::Right => "right",
+        })
     }
 }
