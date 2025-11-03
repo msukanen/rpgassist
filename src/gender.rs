d51a46f25a1815098a10ba0f6c3ca12abe04bd5b
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-03 05:41:48 +0200

    Added 'NeverApplicable' for gender.

--- src/gender.rs
+++ src/gender.rs
@@ -9,6 +9,7 @@ pub enum Gender {
     Unspecified,
     Male,
     Female,
+    NeverApplicable,
 }
 
 impl PartialOrd for Gender {
@@ -25,21 +26,36 @@ where D: Deserializer<'de> {
 }
 
 #[derive(Debug, Deserialize, Clone, Copy)]
+/// A bias value/modifier for dice rolls. What the set value actually
+/// means, depends on [Bias10]'s usage context itself.
 pub struct Bias10 {
     #[serde(deserialize_with = "bias10_filter")]
     value: u32,
 }
 
+impl Default for Bias10 {
+    fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
 #[derive(Debug, Deserialize, Clone, Copy)]
 pub enum GenderBias {
+    /// Male bias. The higher the [Bias10], the more likely result will be [Gender::Male].
     Male(Bias10),
+    /// Approx ⅔ of rolls will result in male.
+    Male23,
+    /// Female bias. The higher the [Bias10], the more likely result will be [Gender::Female].
     Female(Bias10),
+    /// Approx ⅔ of rolls will result in female.
+    Female23,
+    /// No bias one way or the other. About 1:1 distribution between gender choices.
     None
 }
 
 impl Default for GenderBias {
     fn default() -> Self {
-        Self::None
+        Self::None// RL distribution is "close enough" to 1:1.
     }
 }
 
@@ -48,11 +64,22 @@ pub trait HasGenderBias {
 }
 
 impl Gender {
+    /// Generate a random gender.
+    ///
+    /// FYI: Gender distribution is *almost* 1:1 in reality (among humans, that is).
+    /// It is ever so slightly female-biased, but the difference is too small to affect
+    /// dice rolls.
+    pub fn random() -> Self {
+        Self::random_biased(GenderBias::None)
+    }
+
     /// Generate a random gender, with or without bias toward one or the other.
-    pub fn new(bias: GenderBias) -> Self {
+    pub fn random_biased(bias: GenderBias) -> Self {
         if 1.d20() +
         match bias {
+            GenderBias::Male23 => -3,//-3.333 …
             GenderBias::Male(v) => -(v.value as i32),
+            GenderBias::Female23 => 4,//+3.333 …
             GenderBias::Female(v) => v.value as i32,
             _ => 0
         } <= 10 { Self::Male }
@@ -61,8 +88,15 @@ impl Gender {
 
     pub fn get_or_random(&self) -> Self {
         match self {
-            Self::Unspecified => Self::new(GenderBias::None),
+            Self::Unspecified => Self::random_biased(GenderBias::None),
             _ => *self
+        }
+    }
+
+    pub fn resolve_biased(&mut self, bias: GenderBias) {
+        match self {
+            Self::Unspecified => *self = Self::random_biased(bias),
+            _ => ()
         }
     }
 }
@@ -100,7 +134,7 @@ impl Default for Gender {
 impl ResolveInPlace for Gender {
     fn resolve(&mut self) {
         match self {
-            Self::Unspecified => *self = Self::new(GenderBias::None),
+            Self::Unspecified => *self = Self::random_biased(GenderBias::None),
             _ => ()
         }
     }
