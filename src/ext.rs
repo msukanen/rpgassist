83a2db6d85b57e0991b6200e6a6b72eb70c0e643
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-07 18:17:08 +0200

    GetTypeName trait ...

--- src/ext.rs
+++ src/ext.rs
@@ -2,3 +2,5 @@ mod named;
 pub use named::IsNamed;
 mod natural_join;
 pub use natural_join::NaturalJoin;
+mod typename;
+pub use typename::GetTypeName;
---
+++ src/ext/typename.rs
@@ -0,0 +1,9 @@
+use std::any::type_name;
+pub trait GetTypeName{
+    fn type_name(&self) -> &'static str;
+}
+impl <T: ?Sized> GetTypeName for T {
+    fn type_name(&self) -> &'static str {
+        type_name::<T>()
+    }
+}
